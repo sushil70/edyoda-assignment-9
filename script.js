5dbffc624d7551e8ede78ed24b556ef3ff36e2c8
sushil70 <69754620+sushil70@users.noreply.github.com>
2020-12-15 12:56:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
 
 $(document).ready(() => {
-
     function rowCreater(data) {
+        // alert("calling creater function")
 
         const created = `<tr class="data-row" id="${data.id}">
     <td class="column1">${data.id}</td>
@@ -21,8 +21,19 @@ $(document).ready(() => {
 
     }
 
+    // $.get( "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D", function( data,status ) {
+    //     console.log("first api call")
+    //     if(status==="success"){
+    //         var dataCollected = data
+    //         for (var i = 0; i < dataCollected.length; i++) {
 
-    $.get("http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D", function (data) {
+    //             rowCreater(data[i])
+    //         }
+    //     }
+    // });
+
+
+    $.get("https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D", function (data) {
         // $(".result").html(data);
         // alert(data);
         var dataCollected = data
